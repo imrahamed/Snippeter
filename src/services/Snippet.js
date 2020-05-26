e1172a94ab4584b9ffa16e01010d978672eda64b
Gokul Krishnan <gokulkrishnan.p2@gmail.com>
2020-05-26 11:11:40 +0530

    Fixed tag error

--- src/services/Snippet.js
+++ src/services/Snippet.js
@@ -1,32 +1,41 @@
 import { headers } from "./_helpers";
+let ENDPOINT = "http://localhost:5001/api";
 
 export let getAllSnippets = () => {
-    return fetch("http://localhost:5001/api/snippet", { method: "GET", headers: headers() }).then(res => res.json())
+    return fetch(`${ENDPOINT}/snippet`, { method: "GET", headers: headers() }).then(res => res.json())
 }
 
 export let getSnippetsById = (id) => {
-    return fetch(`http://localhost:5001/api/snippet/${id}`, { method: "GET", headers: headers() }).then(res => res.json())
+    return fetch(`${ENDPOINT}/snippet/${id}`, { method: "GET", headers: headers() }).then(res => res.json())
 }
 
 
 export let deleteSnippetsById = (id) => {
-    return fetch(`http://localhost:5001/api/snippet/${id}`, { method: "DELETE", headers: headers() }).then(res => res.json())
+    return fetch(`${ENDPOINT}/snippet/${id}`, { method: "DELETE", headers: headers() }).then(res => res.json())
 }
 
 export const addSnippet = (data) => {
-    return fetch("http://localhost:5001/api/snippet", { method: "POST", headers: headers(), body: JSON.stringify(data) }).then(res => res.json())
+    let tags = data.tags.map(item => item._id);
+    console.log(tags);
+    data.tags = tags;
+    return fetch(`${ENDPOINT}/snippet`, { method: "POST", headers: headers(), body: JSON.stringify(data) }).then(res => res.json())
 }
 
 export const updateSnippet = (data, id) => {
-    return fetch(`http://localhost:5001/api/snippet/${id}`, { method: "PUT", headers: headers(), body: JSON.stringify(data) }).then(res => res.json())
+    let tags = data.tags.map(item => item._id);
+    console.log(tags);
+    data.tags = tags;
+    return fetch(`${ENDPOINT}/snippet/${id}`, { method: "PUT", headers: headers(), body: JSON.stringify(data) }).then(res => res.json())
 }
 
 export const addTags = (data) => {
-    return fetch(`http://localhost:5001/api/tag`, { method: "POST", headers: headers(), body: JSON.stringify({ name: data }) }).then(res => res.json())
+    return fetch(`${ENDPOINT}/tag`, { method: "POST", headers: headers(), body: JSON.stringify({ name: data }) }).then(res => res.json())
 }
 
 export const getTags = (params) => {
-    let url = `http://localhost:5001/api/tag?`;
+    let url = `${ENDPOINT}/tag?`;
     if (params && params.searchText) { url += `search=${params.searchText}` };
     return fetch(url, { method: "GET", headers: headers() }).then(res => res.json())
 }
+
+
--- src/services/_helpers.js
+++ src/services/_helpers.js
@@ -1,3 +1,3 @@
 export let headers = () => {
-    return { "Content-Type": "application/json", "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYzkxNzZhYjg0OGY0MmY0ODkwNTJiMSIsIm5hbWUiOiJHb2t1bCIsImlhdCI6MTU5MDQ2NTcxNiwiZXhwIjoxNTkwNDY5MzE2fQ.DeZStT8Iw7To8sq3yigtP1QnUN5iOvSsgKbHTeTE7y8` };
+    return { "Content-Type": "application/json", "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYzkxNzZhYjg0OGY0MmY0ODkwNTJiMSIsIm5hbWUiOiJHb2t1bCIsImlhdCI6MTU5MDQ3MTQ4OSwiZXhwIjoxNTkwNDc1MDg5fQ.y3ARrdL3CboBxIaVAfjqUg9mTkH50lX_ppDdOGxvNIg` };
 }
